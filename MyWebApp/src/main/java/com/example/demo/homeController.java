4bfe17090c3188fe3c3a2b1b63fd1f25743a1766
Ayush11-8 <Ayush.Goswami@nmbrsystems.com>
2022-02-06 18:24:41 +0530

    ModelAndView object created and worked

--- MyWebApp/src/main/java/com/example/demo/homeController.java
+++ MyWebApp/src/main/java/com/example/demo/homeController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,11 +11,11 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class homeController {
     @RequestMapping("home")
-    public String home(HttpServletRequest req)
+    public ModelAndView home(@RequestParam("name") String myName)
     {
-        HttpSession session = req.getSession();
-        String name= req.getParameter("name");
-        session.setAttribute("name",name);
-        return "home";
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name",myName);
+        mv.setViewName("home");
+        return mv;
     }
 }
